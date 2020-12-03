9c5b33c99d59228677b1359fb881885ae89d7a4c
Or Kalev <orkalev@gmail.com>
2020-12-03 17:40:34 +0200

    missing let rec and pset

--- tag-parser.ml
+++ tag-parser.ml
@@ -81,8 +81,30 @@ let rec tag_parse_exp s = match s with
 (* lambda *)
   |Pair(Symbol("lambda"),Pair(_,Nil)) -> raise X_syntax_error
   |Pair(Symbol("lambda"),Pair(Nil,body)) -> LambdaSimple([],tag_parse_seq body)
-  |Pair(Symbol("lambda"),Pair(Symbol(s),body)) -> LambdaOpt([],s,tag_parse_seq body
+  |Pair(Symbol("lambda"),Pair(Symbol(s),body)) -> LambdaOpt([],s,tag_parse_seq body)
   |Pair(Symbol("lambda"),lambda_expr) -> handle_lambda lambda_expr
+
+(* +  and handle_let_rec sexpr =
++    match sexpr with
++    | Pair(args_list,body) -> (tag_parse (Pair(Symbol("let"), Pair((handle_args_letrec args_list), (handle_body_letrec args_list body)))))
++    | _ -> raise X_syntax_error
++   and handle_args_letrec args_list =
++    match args_list with
++    | Pair(Pair(Symbol(x),a),b) -> Pair(Pair(Symbol(x),Pair(Pair(Symbol("quote") , Pair(Symbol("whatever") , Nil)),Nil)),(handle_args_letrec b))
++    | _ -> Nil
++   and handle_body_letrec args_list body =
++    match args_list with
++    | Pair(Pair(Symbol(x),a),b) -> Pair(Pair(Symbol("set!"), Pair(Symbol(x),a)),(handle_body_letrec b body))
++    | _ -> body *)
+
+
+  |Pair(Symbol("let"),Pair(parms,body)) -> Applic(LambdaSimple(List.map string_of_symbol (get_args parms),tag_parse_seq body),List.map tag_parse_exp (get_vals parms))
+  |Pair(Symbol("let*"),Pair(Nil,body)) -> (tag_parse_exp (Pair(Symbol("let"), Pair(Nil,body))))
+  |Pair(Symbol("let*"),Pair(Pair(args,Nil),body)) -> (tag_parse_exp (Pair(Symbol("let"), Pair(Pair(args,Nil),body))))
+  |Pair(Symbol("let*"),Pair(Pair(car,cdr),body)) -> (tag_parse_exp (Pair(Symbol("let"), Pair(Pair(car,Nil), Pair(Pair(Symbol("let*"),Pair(cdr, body)),Nil)))))
+  (* |Pair(Symbol("lstrec"),Pair(args,body)) -> (tag_parse_exp (Pair(Symbol("let"), Pair((get_args args))))) *)
+  (* |Pair(Symbol("letrec"),Pair(car,cdr)) -> tag_parse_exp *)
+
   (*disjunction*)
   |Pair(Symbol("or"),Nil) -> Const(Sexpr (Bool false))
   |Pair(Symbol("or"),Pair(car_expr,Nil)) -> Const(Sexpr(car_expr))
@@ -93,9 +115,19 @@ let rec tag_parse_exp s = match s with
   |Pair(Symbol("and"),Pair(car_expr,cdr_expr)) -> If((tag_parse_exp car_expr), (tag_parse_exp (Pair(Symbol("and"),cdr_expr))), Const(Sexpr(Bool false)))
 
   (*definitions*)
+  |Pair(Symbol("define"),Pair(Pair(car,cdr),body)) -> (tag_parse_exp (Pair(Symbol("define"),(Pair(car,(Pair(Pair(Symbol("lambda"), Pair(cdr,body)),Nil)))))))
+  |Pair(Symbol("define"),Pair(car_expr,Nil)) -> Def((tag_parse_exp car_expr), Const(Void))
+  |Pair(Symbol("define"),Pair(car_expr,Pair(cdar,Nil))) -> Def((tag_parse_exp car_expr),(tag_parse_exp cdar))
 
   (*assignments*)
+  |Pair(Symbol("set!"), Pair(var, Pair(set_expr,Nil))) -> Set((tag_parse_exp var), (tag_parse_exp set_expr))
+  |Pair(Symbol("pset!"), Pair(var, Pair(set_expr,Nil))) -> Set((tag_parse_exp var), (tag_parse_exp set_expr))
+
+
   (*sequences*)
+  |Pair(Symbol("begin"),Nil) -> Const(Void)
+  |Pair(Symbol("begin"),Pair(car_expr,Nil)) -> tag_parse_exp car_expr
+  |Pair(Symbol("begin"),Pair(car_expr,cdr_expr)) ->Seq(List.map tag_parse_exp (list_of_sexpr (Pair(car_expr,cdr_expr))))
 
 
   (*applications*)
@@ -117,15 +149,20 @@ let rec tag_parse_exp s = match s with
 
   and handle_lambda lambda_expr = 
    match lambda_expr with
-    (* |Pair(_,Nil) -> raise X_syntax_error
-    |Pair(Nil,body))-> LambdaSimple([],tag_parse_seq body)
-    |Pair(Symbol(s),body) -> LambdaOpt([],s,tag_parse_seq body)
-    |Pair(Pair(car,cdr),body) -> LambdaOpt *)
     |Pair(Pair(car,cdr),body) -> 
-      let c_args = List.map string_of_symbol (list_of_sexpr (Pair(car,cdr)) in
+      if (is_proper_list (Pair(car,cdr)))
+      then let c_args = List.map string_of_symbol (list_of_sexpr (Pair(car,cdr))) in
+          if (dupplicate_tester c_args) 
+          then raise X_syntax_error
+          else LambdaSimple(c_args,(tag_parse_seq body))
+      else  
+      let without_last = get_without_last (Pair(car,cdr)) in
+      let last = get_last (Pair(car,cdr)) in
+      if (dupplicate_tester (List.append without_last [last]))
+        then raise X_syntax_error
+        else LambdaOpt(without_last,last,(tag_parse_seq body))
+      |_ -> raise X_syntax_error 
 
-
-    
    
   and tag_parse_seq body = match body with
     |Pair(car,Nil) -> tag_parse_exp car
@@ -134,19 +171,49 @@ let rec tag_parse_exp s = match s with
 
   and is_proper_list list = match list with
     |Nil -> true
-    |pair(car,cdr) -> is_proper_list cdr
+    |Pair(car,cdr) -> is_proper_list cdr
     |_ -> false 
 
   and string_of_symbol s = match s with
     |Symbol(e) -> e
-    |_ -> X_syntax_error 
+    |_ -> raise X_syntax_error 
 
   and dupplicate_tester c_args = match c_args with
   |[] -> false
-  |car :: cdr ->
-  and cdr_containce_car car cdr = match cdr with
-  |x :: y -> if x = y then true else car y 
+  |car :: cdr -> if (cdr_contains_car car cdr) 
+    then true
+    else dupplicate_tester cdr
+  |_ -> raise X_syntax_error 
 
+    
+  and cdr_contains_car car cdr = match cdr with
+  |[] -> false
+  |x :: y -> 
+    if x = car 
+    then true 
+    else cdr_contains_car car y
+  |_ -> raise X_syntax_error 
+
+
+  and get_last args = match args with
+  |Pair(Symbol(x),Symbol(y)) -> y
+  |Pair(Symbol(x), Pair(car,cdr)) -> get_last(Pair(car,cdr))
+  |_ -> raise X_syntax_error
+
+  and get_without_last args = match args with
+  |Pair(Symbol(x),Symbol(y)) -> [x]
+  |Pair(Symbol(x), Pair(car,cdr)) -> List.append [x] (get_without_last (Pair(car,cdr)))
+  |_ -> raise X_syntax_error
+
+  and get_args parms = match parms with
+  |Nil -> []
+  |Pair(Pair(var,_),rest) -> List.append [var] (get_args rest)
+  |_ -> raise X_syntax_error 
+
+  and get_vals parms = match parms with
+  |Nil -> []
+  |Pair(Pair(_,Pair(vals,Nil)),rest) -> List.append [vals] (get_vals rest)
+  |_ -> raise X_syntax_error 
 
   ;;
 
